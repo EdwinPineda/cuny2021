03f302cf380c3b810931c17419a0f9df3499eced
cLee815 <clee8152@gmail.com>
2021-01-24 15:45:03 -0500

    updated landing page

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -8,11 +8,12 @@ export class Home extends Component {
                 <NavBar/>
                 <body>
                     <h1>Welcome to Meet&Clean!</h1>
-                    <p>We are weeeee</p>
                 </body>
                 
-                <h2>We are blah blah blah</h2>
                 <h2>Our goals</h2>
+                <p>Our goals for this orgnanization is to find likeminded individuals who care about our planet. We want to be a way to easily setup volunteer groups and events with people nearby us. On our website you will be able to easily host events at your local parks and beaches. In order to preserve our natural landscapes let's help the Earth together!</p>
+                <h2>Contact Us</h2>
+                <p>For more information or questions contact us at our email.</p>
             </div>
         )
     }
