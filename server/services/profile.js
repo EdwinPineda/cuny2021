616ac9df95f7d4fe059f8cdb8dc008bb543fe956
Zifeng <gcorbett1218@gmail.com>
2021-01-24 00:00:20 -0500

    Added Profile services and controllers

--- server/services/profile.js
+++ server/services/profile.js
@@ -1,1 +1,15 @@
 const db = require("../models");
+
+
+
+const getProfileByuserID = async (data) => {
+    const profile = await db.Profile.findOne({where:{userID: data}});
+
+    return profile;
+}
+
+
+
+module.exports = {
+    getProfileByuserID
+}
